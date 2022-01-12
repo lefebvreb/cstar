3552eb81a63b707353171ab4ba617d3bfbd2a472
Benjamin Lefebvre <benji.lef76@gmail.com>
2022-01-12 13:28:31 +0100

    Fixed structs

--- src/parser/types.rs
+++ src/parser/types.rs
@@ -8,8 +8,7 @@ use super::*;
 pub fn parse_struct_def<'a>(mut pairs: Pairs<'a, Rule>) -> ast::StructDef<'a> {
     let mut def = ast::StructDef::default();
     
-    while let Some(ty) = pairs.next() {
-        let ident = pairs.next().unwrap();
+    while let Some(ident) = pairs.next() {
         def.names.insert(ident.as_str());
     }
 
